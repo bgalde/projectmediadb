2b44a770b5a61767ccc06341c76b5d31ea3f3325
Yodit <yodittesfaye14@gmail.com>
2018-12-08 18:39:18 -0500

    Test Commit
    Uploading Test code

---
+++ src_1/src/test/java/edu/umuc/cmsc495/SrcApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.umuc.cmsc495;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SrcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
